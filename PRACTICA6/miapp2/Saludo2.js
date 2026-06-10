4e381ccf42e73f860e4f87ffb86591c7017decc1
aaron-sccell <prog.sanchez.aaron@gmail.com>
2026-06-10 07:30:06 -0600

    S2

--- PRACTICA6/miapp2/Saludo2.js
+++ PRACTICA6/miapp2/Saludo2.js
@@ -1,13 +1,33 @@
-import { Button, Image, Texto } from "react-native";
-import { View } from "react-native";
+import { Button, Image, Text, View, StyleSheet, Alert } from 'react-native';
 
 export const Saludo2 = () => {
-    return (
+  return (
+    <View style={styles.tarjeta}>
+      <Image source={require('../assets/wave.png')} style={styles.imagen} />
+      <Text style={styles.texto}>Soy un componente compuesto</Text>
+      <Button title="Hola desde Saludo2" onPress={() => Alert.alert('Saludo2', 'Componente funcionando')} />
+    </View>
+  );
+};
 
-        <View>
-          <Image source={require('./assets/wave.png')} />  
-        <Text> Soy un componente compuesto  </Text>
-        <Button title="Hola 203"> </Button>
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  tarjeta: {
+    marginTop: 20,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    alignItems: 'center',
+    width: '90%',
+  },
+  imagen: {
+    width: 60,
+    height: 60,
+    marginBottom: 10,
+  },
+  texto: {
+    fontSize: 16,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+});
